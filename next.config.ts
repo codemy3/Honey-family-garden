f81610bb67d65413f41591c0b6f357c199dd666e
Maithri <smaithri039@gmail.com>
2026-07-18 15:32:30 +0530

    fix: allow sanity cdn domains for next image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.7'],
   serverExternalPackages: ['sanity', '@sanity/icons'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
