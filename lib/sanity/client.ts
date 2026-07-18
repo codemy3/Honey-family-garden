ce20ea176a417745b59d11a6d749af6bdf61ae81
Maithri <smaithri039@gmail.com>
2026-07-18 12:23:41 +0530

    fix: hardcode sanity projectId fallback for vercel build

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "edh8ol10";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const apiVersion = "2024-01-01"; // Use current date for the API version
 
 export const client = createClient({
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schema } from "./schemaTypes";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "edh8ol10";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
   basePath: "/studio",
